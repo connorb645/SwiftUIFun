58b8834fe65bfe2795d21cbb5042336b39bac35e
Connor Black <connor.black@notifytechnology.com>
2020-12-10 21:09:21 +0000

    - Added project to repo

---
+++ SwiftUIFun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIFun
+//
+//  Created by Connor Black on 10/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIFun/SwiftUIFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFunApp.swift
+//  SwiftUIFun
+//
+//  Created by Connor Black on 10/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
